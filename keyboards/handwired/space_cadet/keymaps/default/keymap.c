08d6ad54da4f6c54748f34848b7127b4b40cd156
ajshell1 <11084981+ajshell1@users.noreply.github.com>
2022-01-06 15:14:09 -0500

    Preliminary original layout.

--- keyboards/handwired/space_cadet/keymaps/default/keymap.c
+++ keyboards/handwired/space_cadet/keymaps/default/keymap.c
@@ -27,6 +27,7 @@ enum custom_keycodes {
     QMKURL
 };
 /*
+*  F1             F2           F3          F4          F5         F6         F7        F8            F9        F10          F11           F12
 *┌───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┬───────────┐
 *│MACRO      │TERMINAL   │QUOTE      │OVERSTRIKE │CLEAR INPUT│CLEARSCREEN│HOLD OUTPUT│STOP OUTPUT│ABORT      │BREAK      │RESUME     │CALL       │
 *├─────┬─────┼───────────┼─────┬─────┼─────┬─────┼─────┬─────┼─────┬─────┼─────┬─────┼─────┬─────┴──┬────────┼─────┬─────┼───────────┼─────┬─────┤
@@ -38,14 +39,19 @@ enum custom_keycodes {
 *├─────┬─────┴──┬────────┼─────────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──────────┬┴───────┼────────┬──┴─────┬─────┤
 *│CAPS │TOP     │GREEK   │SHIFT       │Z    │X    │C    │V    │B    │N    │M    │,/<  │./>  │//?  │SHIFT        │GREEK   │TOP     │REPEAT  │MLOCK│
 *├─────┴──┬─────┴──┬─────┴──┬─────────┼──┬──┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴──┬──┼─────────┬───┴─────┬──┴─────┬──┴─────┬──┴─────┤
-*│HYPER   │SUPER   │META    │CONTROL  │  │                                                     │  │CONTROL  │         │HYPER   │SUPER   │META    │
+*│HYPER   │SUPER   │META    │CONTROL  │  │                                                     │  │CONTROL  │         │META    │SUPER   │HYPER   │
 *└────────┴────────┴────────┴─────────┘  └─────────────────────────────────────────────────────┘  └─────────┘         └────────┴────────┴────────┘
 */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
+        
+        KC_F1,            KC_F2,   KC_F3,            KC_F4,            KC_F5,            KC_F6,            KC_F7,            KC_F8,            KC_F9,            KC_F10,  KC_F11,           KC_F12,
+        KC_P1,   KC_P2,   KC_PGUP, KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_NO,   KC_HOME, KC_UP,   KC_DOWN,
+        KC_P3,   KC_P4,   KC_INS,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_GRV,  KC_BSLS, KC_DEL,  KC_LEFT, KC_RGHT,
+        KC_SCRL,          KC_PGDN, KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_NO,   KC_END,           KC_NO,  
+        KC_CAPS, MO(TOP), MO(GRK), KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(GRK),          MO(TOP), KC_NO,            KC_NO,
+        KC_HYPR, KC_LGUI, KC_LALT, KC_LCTL,                                              KC_SPACE,                                    KC_RCTL,                            KC_RALT, KC_RGUI, KC_HYPR
     ),
     [_FN] = LAYOUT(
         QMKBEST, QMKURL,  _______,
